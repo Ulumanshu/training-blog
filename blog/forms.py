b7af10feddc94d41f963d8b37433261f397d96b7
Ulumanshu <karolis_kesminas@yahoo.com>
2020-10-24 09:41:34 +0300

    Change excluded words text

--- blog/forms.py
+++ blog/forms.py
@@ -9,7 +9,7 @@ class PostForm(forms.ModelForm):
 
     def clean_text(self):
         text  = self.cleaned_data.get('text')
-        badwords = ['fuck', 'kaka', 'audi']
+        badwords = ['fuck', 'kaka', 'adsvgergewrg']
         check_list = [True if w in text else False for w in badwords]
         # if title exists create slug from title
         if True in check_list:
